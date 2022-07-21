1a34dbfd3c74f4669e5d74984707a0aded21156c
Will Morcombe <sc20wtm@leeds.ac.uk>
2022-07-21 22:16:01 +0100

    added more to about section

---
+++ src/constants/about_data.js
@@ -0,0 +1,6 @@
+const text_content = {
+  para : 'I am a cheerful and enthusiastic recent masters graduate achieving a distinction in Advanced Computer Science with Artificial Intelligence that is driven by a constant desire to learn and innovate. I am looking to secure a position in software engineering, data science, machine learning or similar. Long term interests in technology blogs, especially in machine learning, such as the OpenAI blog, the DeepMind blog, and the YouTube channel, two-minute papers are constantly examined to keep me up to date with the growth and advances in technology.'
+};
+
+
+export default text_content;
